31b4d21e437c09aa6faa11ccb1cc6389d7511a25
Peter Dekkers <425971+PeterDekkers@users.noreply.github.com>
2025-12-19 12:53:12 +1300

    Fix main script

--- src/main.py
+++ src/main.py
@@ -1,10 +1,46 @@
 from __future__ import annotations
 
+import re
 import argparse
 import os
+import subprocess
 from collections.abc import Sequence
+from typing import Any
 
-from pre_commit_hooks.util import cmd_output
+UFO_PATH_RE = r".*.ufo"
+
+
+def get_ufo_path_from_diff_line(diff_line: str) -> str | None:
+    diff_line = diff_line.encode().decode('unicode-escape').encode('latin1').decode(
+        'utf-8').strip().lstrip('"').rstrip('"')
+    matches = re.search(UFO_PATH_RE, diff_line, re.IGNORECASE)
+    if not matches:
+        return None
+    return matches.group(0)
+
+
+def get_ufo_paths_from_diff(git_diff: str) -> set[str]:
+    """
+    Gets all unique UFO paths from a raw git diff.
+    """
+    ufo_paths: set[str] = set()
+    for diff_line in git_diff.splitlines():
+        ufo_path = get_ufo_path_from_diff_line(diff_line)
+        if not ufo_path:
+            continue
+        ufo_paths.add(ufo_path)
+    return ufo_paths
+
+
+def call_subprocess(*cmd: str, **kwargs: Any) -> str:
+    kwargs.setdefault('stdout', subprocess.PIPE)
+    kwargs.setdefault('stderr', subprocess.PIPE)
+    proc = subprocess.Popen(cmd, **kwargs)
+    stdout, stderr = proc.communicate()
+    stdout = stdout.decode()
+    if proc.returncode:
+        raise RuntimeError(cmd, proc.returncode, stdout, stderr)
+    return stdout
 
 
 def main(argv: Sequence[str] | None = None) -> int:
@@ -14,39 +50,24 @@ def main(argv: Sequence[str] | None = None) -> int:
         action='store_true',
         dest='autofix',
         help='Automatically format UFOs',
+        default=False
     )
+
     args = parser.parse_args(argv)
 
-    if (
-        'PRE_COMMIT_FROM_REF' in os.environ and
-        'PRE_COMMIT_TO_REF' in os.environ
-    ):
-        diff_arg = '...'.join((
-            os.environ['PRE_COMMIT_FROM_REF'],
-            os.environ['PRE_COMMIT_TO_REF'],
-        ))
-    else:
-        diff_arg = '--staged'
-    added_diff = cmd_output(
-        'git', 'diff', '--diff-filter=A', '--raw', diff_arg, '--',
-        *args.filenames,
-    )
-    retv = 0
-    for line in added_diff.splitlines():
-        metadata, filename = line.split('\t', 1)
-        new_mode = metadata.split(' ')[1]
-        if new_mode == '160000':
-            print(f'{filename}: new submodule introduced')
-            retv = 1
+    # Get the filenames of the staged UFOs
+    commands = 'git', 'diff', '--name-only', '--raw', '--staged', '--', '*.ufo/*'
+    added_diff = call_subprocess(*commands)
 
-    if retv:
-        print()
-        print('This commit introduces new submodules.')
-        print('Did you unintentionally `git add .`?')
-        print('To fix: git rm {thesubmodule}  # no trailing slash')
-        print('Also check .gitmodules')
+    # Run ufoNormalizer on each staged UFO
+    for ufo_path in get_ufo_paths_from_diff(added_diff):
+        call_subprocess('ufonormalizer', '--all', '--quiet', '--no-mod-times', ufo_path)
 
-    return retv
+        # If fixing, add the fixed UFO back to the commit
+        if args.autofix:
+            call_subprocess('git', 'add', '--update', ufo_path)
+
+    return 0
 
 
 if __name__ == '__main__':
